6e271ee4900739843fae8eb69ead3e52fdeb2865
Alexander <maniacallion@gmail.com>
2019-04-15 17:54:31 -0500

    5 passes

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -81,7 +81,14 @@ This anonymous function should accept up to three arguments: the element, the in
 ------------------------------------------------------------------------------------------------ */
 
 const removeWithAnon = (arr) => {
-  // Solution code here...
+  arr.forEach(function(element, index, array) {
+    if (element % 3 === 2) {
+      arr.pop();
+    }
+    else {
+    }
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -99,7 +106,7 @@ This function should use forEach to populate your grocery list based on the stor
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
-  // Solution code here...
+
 };
 
 /* ------------------------------------------------------------------------------------------------
