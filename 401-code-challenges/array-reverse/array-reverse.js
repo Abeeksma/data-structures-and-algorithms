31ffd5c203c3c330dc146ea98f8d0d573aab896c
Alexander <maniacallion@gmail.com>
2019-05-20 21:35:11 -0500

    reworking shit to hopefully figure out this travis

--- 401-code-challenges/array-reverse/array-reverse.js
+++ 401-code-challenges/array-reverse/array-reverse.js
@@ -3,17 +3,25 @@
 let array = [1, 2, 3, 4, 5, 6];
 
 function reverseArray(array) {
+  if(!Array.isArray(array)){
+    return 'cannot reverse an array if you don\'t give me an array';
+  }
+  else if (array.length === 0){
+    return [];
+  }
+  else {
   for (let i = 0; i <= Math.floor((array.length - 1)/2); i++) {
       let swappingNumber = array[i];
       array[i] = array[array.length - 1 - i];
       array[array.length - 1 - i] = swappingNumber;
   }
   return array;
+  }
 };
 
 console.log(reverseArray(array));
 
-//don't run them both in a row as the first returns the same array reversed and the second reverses the reverse
+
 
 function anotherReverseArray (array) {
   let reversedArray = [];
