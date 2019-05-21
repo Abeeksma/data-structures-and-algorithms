8f9898220dd92cd0e974624314534506d5972f53
Alexander <maniacallion@gmail.com>
2019-05-20 19:48:52 -0500

    two reverse array functions

---
+++ 401-code-challenges/array-reverse/README.md
@@ -0,0 +1,13 @@
+# Reverse an Array
+an array must be reversed without using methods
+
+## Challenge
+given an array i must reverse whats in the array 
+
+## Approach & Efficiency
+first reverse i wanted to swap the first with the last until i reached the middle, while keeping the same array. 
+
+second pushes the numbers from the end of the first array to the new array
+
+## Solution
+<!-- Embedded whiteboard image -->
--- 401-code-challenges/array-reverse/array-reverse.js
+++ 401-code-challenges/array-reverse/array-reverse.js
@@ -1,3 +1,26 @@
 'use strict'
 
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function reverseArray(array) {
+  for (let i = 0; i <= Math.floor((array.length - 1)/2); i++) {
+      let swappingNumber = array[i];
+      array[i] = array[array.length - 1 - i];
+      array[array.length - 1 - i] = swappingNumber;
+  }
+  return array;
+};
+
+console.log(reverseArray(array));
+
+function anotherReverseArray (array) {
+  let reversedArray = [];
+  for(let i = array.length -1; i >= 0; i--) {
+    reversedArray.push(array[i]);
+  }
+  return reversedArray;
+};
+
+console.log(anotherReverseArray(array));
+
 
