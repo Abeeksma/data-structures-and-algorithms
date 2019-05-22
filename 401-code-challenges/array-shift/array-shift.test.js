fdfa88543ff98933a29b5d2d1d3e046bba045930
Alexander <maniacallion@gmail.com>
2019-05-21 20:33:00 -0500

    third test passes

--- 401-code-challenges/array-shift/array-shift.test.js
+++ 401-code-challenges/array-shift/array-shift.test.js
@@ -1,28 +1,43 @@
 'use strict'
 
-function insertShiftArray(array, middleInsert){
+function insertShiftArray(array, insert){
   if (!Array.isArray(array)){
     return 'I need an array to put something into'}
-  else if (typeof middleInsert !== 'number' && typeof middleInsert !== 'string'){
+  else if (typeof insert !== 'number' && typeof insert !== 'string'){
     return array;
   }
-  else{
-    let middleFinger = Math.ceil(arr.length/2)
-    let insertedShiftedArray = []
-    for(let i = 0; i < array.length; i++) {
-
+    let insertedArray = [];
+    let middleFinger = Math.ceil(array.length / 2);
+    
+    for(let i = 0; i < array.length + 1; i++) {
+      if(i < middleFinger){
+        insertedArray[i]  = array[i];
+      }
+      else if (i > middleFinger) {
+        insertedArray[i] = array[i - 1];
+      }
+      else{ 
+        insertedArray[middleFinger] = insert;
+      }
     }
-  return array;
-  }
+    return insertedArray;
 };
-
-console.log(insertShiftArray(array4, middleInsert));
 
 
 describe('Array with value added to middle', () => {
  it('Is not an array', () => {
-  const result = insertShiftArray(array4, middleInsert);
-  expect(result).toBe(null)
+  let result = insertShiftArray('array', 7);
+  expect(result).toBe('I need an array to put something into')
  });
-});
 
+ it('Does not have something to insert', () => {
+   let result = insertShiftArray([1, 2, 3, 4, 5], undefined);
+   expect(result).toEqual([1, 2, 3, 4, 5]);
+ });
+
+ it('Is an odd array of numbers with a number inserted inserted in the middle', () => {
+  let result = insertShiftArray([4, 6, 5, 3, 5, 9, 3], 2);
+  expect(result).toEqual([4, 6, 5, 3, 2, 5, 9, 3]);
+ });
+
+});
