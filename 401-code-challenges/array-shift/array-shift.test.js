741ad397261910bbae3956f86ea56f2c51d36594
Alexander <maniacallion@gmail.com>
2019-05-21 20:40:33 -0500

    7 passing tests

--- 401-code-challenges/array-shift/array-shift.test.js
+++ 401-code-challenges/array-shift/array-shift.test.js
@@ -6,6 +6,9 @@ function insertShiftArray(array, insert){
   else if (typeof insert !== 'number' && typeof insert !== 'string'){
     return array;
   }
+  else if (array.length === 0){
+    return [insert];
+  }
     let insertedArray = [];
     let middleFinger = Math.ceil(array.length / 2);
     
@@ -35,9 +38,23 @@ describe('Array with value added to middle', () => {
    expect(result).toEqual([1, 2, 3, 4, 5]);
  });
 
- it('Is an odd array of numbers with a number inserted inserted in the middle', () => {
+ it('Is an empty array with an insert value', () => {
+   let result = insertShiftArray([], 3);
+   expect(result).toEqual([3]);
+ });
+
+ it('Is an odd array of numbers with a number inserted into the middle', () => {
   let result = insertShiftArray([4, 6, 5, 3, 5, 9, 3], 2);
   expect(result).toEqual([4, 6, 5, 3, 2, 5, 9, 3]);
  });
 
+ it('is an even array of number with a number inserted into the middle', () => {
+  let result = insertShiftArray([1, 2, 3, 4], 9);
+  expect(result).toEqual([1, 2, 9, 3, 4]);
+ });
+
+ it('is an array of string and numbers with a number inserted', () => {
+   let result = insertShiftArray([1, 'two', 3, 'four'], 5);
+   expect(result).toEqual([1, 'two', 5, 3, 'four']);
+ })
 });
