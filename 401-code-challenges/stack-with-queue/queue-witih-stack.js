3f7f18e5ac0a865f5ee4559e1a8f120e20ef9334
Alexander <maniacallion@gmail.com>
2019-06-03 15:11:21 -0500

    plan for queues with stack commented in have to finish the stack and queue lab first though so i have a pop/push

--- 401-code-challenges/stack-with-queue/queue-witih-stack.js
+++ 401-code-challenges/stack-with-queue/queue-witih-stack.js
@@ -7,3 +7,13 @@ class PsuedoQueue {
 
   }
 }
+
+/* the plan is to have a stack1
+if i want to enqueue something push it to stack1
+if i want to dequeue something i pop then push each item to stack2
+pop node, push node, repeat until stack 1 is empty
+pop node off the stack2, 
+then move everything back to stack1
+pop push pop push ext ext 
+*/
+
