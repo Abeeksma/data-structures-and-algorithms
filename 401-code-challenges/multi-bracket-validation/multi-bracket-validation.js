5e1259fcf0abed7e7ed26a7c8865f9e8c3ecc3f8
Alexander <maniacallion@gmail.com>
2019-06-05 14:24:30 -0500

    function starting to take shape, split the array, filter out the brackets

---
+++ 401-code-challenges/multi-bracket-validation/multi-bracket-validation.js
@@ -0,0 +1,82 @@
+'use strict'
+
+// the plan!!
+// make two arrays, one for opening brackets one for closing brackets
+// split input into an array
+// for each thing in the input/split array check if it exists in the bracket arrays
+// pop i check i put it on a stack 
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(value) {
+    if (!value) {
+      return `You must pass in a value to add`;
+    }
+
+    const newNode = new Node(value);
+    if (!this.top) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+  }
+
+  pushMultiple(arr) {
+    if (!arr) {
+      return `You must pass in at least two values`;
+    }
+
+    arr.forEach((value) => {
+      const newNode = new Node(value);
+      if (!this.top) {
+        this.top = newNode;
+      } else {
+        newNode.next = this.top;
+        this.top = newNode;
+      }
+    });
+  }
+
+  pop() {
+    if (!this.top) {
+      return `You dont have anything in your stack!`;
+    }
+
+    let removedNode = this.top;
+    this.top = this.top.next;
+    removedNode.next = null;
+    return removedNode;
+  }
+
+  peek() {
+    if (!this.top) {
+      return `You dont have anything in your stack!`;
+    }
+
+    return this.top.value;
+  }
+}
+
+////stack and node constructor ^^^^^^^^^6
+
+/// now for work ///
+
+let openingBrackets = ['(', '[', '{'];
+let closingBrackets = [')', ']', '}'];
+
+bracketRacket = function(array){
+  const splitArray = array.split('');
+
+
+}
