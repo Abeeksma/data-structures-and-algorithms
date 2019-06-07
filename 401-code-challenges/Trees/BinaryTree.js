000ff2cdd2ae1683b1be48dd210e54bc6282aefc
Alexander <maniacallion@gmail.com>
2019-06-07 13:15:12 -0500

    base constructor for node and Binary Tree, no methods yet

---
+++ 401-code-challenges/Trees/BinaryTree.js
@@ -0,0 +1,15 @@
+'use strict';
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.left = this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  
+}
