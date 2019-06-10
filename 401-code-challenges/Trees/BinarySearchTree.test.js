52bc1f6355265bdd86815e2bae3c0ba427cef478
Alexander <maniacallion@gmail.com>
2019-06-10 09:37:03 -0500

    i am bad at doing tests first

--- 401-code-challenges/Trees/BinarySearchTree.test.js
+++ 401-code-challenges/Trees/BinarySearchTree.test.js
@@ -1,3 +1,24 @@
 'use strict';
 
 const BinarySearchTree = require('./BinarySearchTree');
+
+
+
+function testTree() {
+
+  var tree = new BinaryTree();
+
+  BinarySearchTree.add(15);
+  BinarySearchTree.add(25);
+  BinarySearchTree.add(10);
+  BinarySearchTree.add(7);
+  BinarySearchTree.add(22);
+  BinarySearchTree.add(17);
+  BinarySearchTree.add(13);
+  BinarySearchTree.add(5);
+  BinarySearchTree.add(9);
+  BinarySearchTree.add(27);
+
+  return tree;
+
+}
