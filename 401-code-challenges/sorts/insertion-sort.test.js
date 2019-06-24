24be8479150c65a68111db5bfc22199a397f271d
Alexander <maniacallion@gmail.com>
2019-06-24 16:15:58 -0500

    a couple more tests

--- 401-code-challenges/sorts/insertion-sort.test.js
+++ 401-code-challenges/sorts/insertion-sort.test.js
@@ -5,6 +5,8 @@ let insertionSort = require('./insertion-sort');
 let testArray = [9, 2, 5, 10, 1, 3, 8, 7, 6, 4];
 let testTwo = [9, 5, 1, 1, 2, 4, 4, 29];
 let testThree = [100, -1, 2, 5,  0, 7, 9, -4];
+let testFour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let testFive = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 describe('Insertion Sort', () => {
   it('should sort an array from least to greatest', () => {
@@ -18,4 +20,13 @@ describe('Insertion Sort', () => {
   it('shoul sort with negative numbers', () => {
     expect(insertionSort(testThree)).toEqual([-4, -1, 0, 2, 5, 7, 9, 100])
   });
+
+  it('should sort an inorder array', () => {
+    expect(insertionSort(testFour)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  it('should sort a reversed array', () => {
+    expect(insertionSort(testFive)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
 });
