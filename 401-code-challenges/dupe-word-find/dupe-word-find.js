823697be891e0ccd78efca2c27154dff1b112134
Alexander <maniacallion@gmail.com>
2019-07-08 17:28:12 -0500

    added the plan of action so i don't lose my way whilst i work

--- 401-code-challenges/dupe-word-find/dupe-word-find.js
+++ 401-code-challenges/dupe-word-find/dupe-word-find.js
@@ -1,2 +1,18 @@
 'use strict';
 
+/*
+
+The plan to return the first repeated word in a lengthy string of words
+
+so first off we make a function that takes in a string
+i need to turn all of the string to lowercase
+split the string on spaces be able to access all the words
+remove special characters (only from the end of words as i need to check for it's and such)
+loop over array of my split up string 
+for each word i want to hash the word
+if the hashed word is in the table return the word
+if not add the hashed word to the table and increment through the words
+if no reapeted words return 'no words repeated' 
+
+*/
+
