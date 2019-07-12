155f6673ea738d6e38bafb19d1aef54b49bffabc
Alexander <maniacallion@gmail.com>
2019-07-12 15:33:00 -0500

    untested functionality for addNode, addEdge, addEdgeDirected, getNodes

--- 401-code-challenges/graphs/graph.js
+++ 401-code-challenges/graphs/graph.js
@@ -3,25 +3,41 @@
 class Graph {
   constructor(){
     this.nodes = [],
-    this.edges = {}
+    this.edges = {},
+    this.nodeCount = 0,
+    this.edgeCount = 0,
   }
 
   addNode(node) {
-    
+    this.nodes.push(node);
+    this.edges[node] = [];
+    this.nodeCount += 1; 
   }
 
-  addEdge() {
+  addEdge(node1, node2, weight = 1) {
+    this.edges[node1].push({node: node2, weight});  
+    this.edges[node2].push({node: node1, weight});
+    this.edgeCount +=1;
+  }
 
+  addEdgeDirected(node1, node2, weight = 1){
+    this.edges[node1].push({node: node2, weight});
+    this.edgeCount += 1; 
   }
 
   getNodes() {
-
+    let nodeCollection = [];
+    for( let i = 0; i < this.nodes.length; i ++ ) {
+      nodeCollection.push(this.nodes[i])
+    }
+    return nodeCollection;
   }
 
   getNeighbors() {
 
   }
 
+  //number of nodes
   size() {
 
   }
