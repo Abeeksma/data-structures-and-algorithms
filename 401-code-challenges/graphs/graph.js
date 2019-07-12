642612a2b6879685e18a33dcc1b98969f27113a2
Alexander <maniacallion@gmail.com>
2019-07-12 14:40:26 -0500

    layout of the base progression

---
+++ 401-code-challenges/graphs/graph.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class Graph {
+  constructor(){
+    this.nodes = [],
+    this.edges = {}
+  }
+
+  addNode(node) {
+    
+  }
+
+  addEdge() {
+
+  }
+
+  getNodes() {
+
+  }
+
+  getNeighbors() {
+
+  }
+
+  size() {
+
+  }
+
+}
+
+module.exports = Graph;
---
+++ 401-code-challenges/graphs/graph.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Graph = require('./graph');
+
+describe('The Graph', () => {
+  it('can add a node to the graph', () => {
+
+  });
+
+  it('can successfully add an edge to the graph', () => {
+
+  });
+
+  it('can retrieve a collection of nodes from the graph', () => {
+
+  });
+
+  it('can retrieve all apporiate neighbors from the graph', () => {
+
+  });
+
+  it('can retun neighbors with the weight between nodes included', () => {
+
+  });
+
+  it('can return the size of graphs (the number of nodes)', () => {
+
+  });
+
+  it('can return a graph that only has one node and one edge', () => {
+
+  });
+
+  it('will return (something usefull) for an empyt graph', () => {
+
+  });
+})
